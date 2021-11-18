64c59e753cc9974d7a3ecbcd3a9cab2f1a2bf9c7
StanleyRabkin <stanley.rabkin23@sitechhs.com>
2021-11-18 11:56:20 -0500

    rityouip['oiuy

--- vueapi/src/views/Home.vue
+++ vueapi/src/views/Home.vue
@@ -1,13 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
+    <episodeList />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 
+import episodeList from "../views/About.vue";
 export default {
-      
+      name: "Home",
+      components: {
+        episodeList,
+      },
 };
 </script>
